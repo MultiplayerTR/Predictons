d899c8126ec920e761da62a527ae727b9ba11f15
AtiErdal7 <atakan@multiplayer.com.tr>
2024-06-25 14:29:25 +0300

    players now gain points from predictions

--- src/ScrollContainerVerticalForLeaderboard.tsx
+++ src/ScrollContainerVerticalForLeaderboard.tsx
@@ -40,32 +40,61 @@ const ScrollContainerVerticalForLeaderboard: React.FC<ListProps>= ({height,items
     useEffect(() => {
         getUserData();
         getPredictionData();
-    }, [getPredictionData, getUserData]);
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
             let userD:string[][] = [];
             try {
                 for (let i = 0; i < userData.length; i++) {
-                    let user: string[] = ['Guest', '0', '0', '0'];
+                    let user: string[] = [];
+                    let username = "Guest";
+                    if (userData[i].nickname !== undefined){
+                        username = userData[i].nickname;
+                    }
+                    let correctGuesses = 0;
+                    let points = 0;
+                    let predictions = 0;
                     for (let j = 0; j < items.length; j++) {
                         for (let k = 0; k < predictionData.length; k++) {
+                            console.log(predictionData[k].id)
+                            console.log(userData[i].id)
                             // @ts-ignore
                             if (predictionData[k].id === items[j].HOME_NAME + items[j].AWAY_NAME + userData[i].id) {
-                                user = [userData[i].nickname, predictionData.length.toString(), '3', '9'];
+                                predictions++;
+                                // @ts-ignore
+                                if (predictionData[k].prediction1 === items[j].HOME_SCORE_CURRENT && predictionData[k].prediction2 === items[j].AWAY_SCORE_CURRENT){
+                                    correctGuesses++;
+                                    points+=5;
+                                }
+                                else{
+                                    // @ts-ignore
+                                    if ((predictionData[k].prediction1-predictionData[k].prediction2 === items[j].HOME_SCORE_CURRENT-items[j].AWAY_SCORE_CURRENT)){
+                                        points+=3;
+                                    }
+                                    // @ts-ignore
+                                    else if((predictionData[k].prediction1>predictionData[k].prediction2 &&items[j].HOME_SCORE_CURRENT>items[j].AWAY_SCORE_CURRENT) || (predictionData[k].prediction1<predictionData[k].prediction2 &&items[j].HOME_SCORE_CURRENT<items[j].AWAY_SCORE_CURRENT)|| (predictionData[k].prediction1===predictionData[k].prediction2 &&items[j].HOME_SCORE_CURRENT===items[j].AWAY_SCORE_CURRENT)){
+                                        points+=1
+                                    }
+                                }
+
                             }
                         }
                     }
+                    user = [username, predictions.toString(), correctGuesses.toString(), points.toString()];
                     userD.push(user);
                 }
             } catch (err) {
                 console.log(err);
             }
+
+            userD.sort((a, b) => parseInt(b[3]) - parseInt(a[3]));
+
             setUserScores(userD);
         };
 
         fetchData();
-    }, [items]);
+    }, [items, predictionData, userData]);
 
         return (
             <>
